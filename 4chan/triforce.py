a0fc302cb41f121a5aa752baf3ebd1cb03e9b719
theeyed <alex.m.herring@gmail.com>
2013-09-21 20:51:37 -0400

    4chan
    Added triforce script

---
+++ 4chan/triforce.py
@@ -0,0 +1,27 @@
+import sys
+
+def main(arg, SPACE = '-', TRI = 'Z'):
+    if len(arg) == 2:
+        try:
+            tri = int(arg[1])
+        except:
+            print 'nice try'
+            return 'bad kid'
+    else:
+        print 'Usage: trifoce -num'
+        return 'retawd'
+    tri = abs(tri + (tri % 2) - 1)
+    part, i = [], 1
+    while i <= tri:
+        part += [SPACE * ((tri - i)/2) + TRI * i + SPACE * ((tri - i)/2)]
+        i += 2
+    part += part
+    for i in range(len(part)/2):
+        part[i] = SPACE * ((len(part[i])+1)/2) + part[i] + SPACE * ((len(part[i])+1)/2)
+        print part[i]
+    for i in range(len(part)/2, len(part)):
+        part[i] += SPACE + part[i]
+        print part[i]
+
+if __name__ == '__main__':
+    main(sys.argv)
